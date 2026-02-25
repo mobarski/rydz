1e5ca38af7a19cbac76fc71be1b35a4376af5658
Maciej Obarski <mobarski@gmail.com>
2026-02-25 22:24:01 +0100

    WIP thinking models (almost there)

--- src/rydz/logprobs.py
+++ src/rydz/logprobs.py
@@ -19,7 +19,7 @@ def _get_response_from_responses(model, prompt, **kwargs):
         input=prompt,
         temperature=quirks.get('temperature', 0.0),
         top_logprobs=quirks.get('top_logprobs', 20),
-        max_output_tokens=quirks.get('max_tokens', 4096 if quirks['thinking'] else 1),
+        max_output_tokens=quirks.get('max_tokens', 4096 if quirks['thinking'] else 1), # TODO: thinking vs provider quirk
         include=['message.output_text.logprobs'],
     )
     if quirks['thinking']:
@@ -52,7 +52,7 @@ def _get_response_from_chat(model, prompt, **kwargs):
         model=model_name(model),
         messages=[{"role": "user", "content": prompt}],
         temperature=quirks.get('temperature', 0.0),
-        max_tokens=quirks.get('max_tokens', 4096 if quirks['thinking'] else 1),
+        max_tokens=quirks.get('max_tokens', 4096 if quirks['thinking'] else 1), # TODO: thinking vs provider quirk
         logprobs=quirks.get('logprobs', True),
         top_logprobs=quirks.get('top_logprobs', 20),
         #reasoning_effort='none',
@@ -88,31 +88,6 @@ def get_probability(resp, answer):
         if answer.upper().startswith(t):
             p_total += exp(x.logprob)
     return min(1.0, p_total)
-
-### WIP - THINKING MODELS ##############################################################
-
-# REF: https://developers.openai.com/api/reference/resources/responses/methods/create
-def _get_response_from_responses_thinking(model, prompt, **kwargs):
-    client = _get_client(model)
-    provider = model.partition(':')[0]
-    quirks = QUIRKS.get(provider, {}).copy()
-    quirks.update(kwargs)
-    thinking = quirks.get('thinking', True)
-    t0 = time.perf_counter()
-    resp = client.responses.create(
-        model=model_name(model),
-        input=prompt,
-        temperature=quirks.get('temperature', 0.0),
-        top_logprobs=quirks.get('top_logprobs', 20),
-        #max_output_tokens=10,
-        include=['message.output_text.logprobs'],
-        text={"verbosity": "low", "format": {"type": "text"}} if quirks.get('thinking') else None,
-        reasoning={"effort": "minimal", "summary": "concise"} if quirks.get('thinking') else None,
-    )
-    resp.aux = types.SimpleNamespace()
-    all_logprobs = resp.output[0].content[0].logprobs
-    resp.aux.logprobs = _get_top_logprobs_skipping_thinking_tokens(all_logprobs)
-    return resp
 
 
 def _get_top_logprobs_skipping_thinking_tokens(logprobs):
